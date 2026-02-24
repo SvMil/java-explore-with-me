d72b1e4ce40cd32d8f8b1afa6017d06cd088543e
SvMil <lana.milovidova@yandex.ru>
2026-02-25 02:31:58 +0300

    fix stat_svc branch

--- stats/client/src/main/java/ru/practicum/ewm/stats/client/Client.java
+++ stats/client/src/main/java/ru/practicum/ewm/stats/client/Client.java
@@ -9,33 +9,29 @@ import org.springframework.core.ParameterizedTypeReference;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @RequiredArgsConstructor
 public class Client {
     private RestClient restClient;
 
-    public List<StatsViewDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        if (start == null || end == null) {
-            throw new IllegalArgumentException("Ошибка валидации. Даты начала и окончания должны не могут быть пустыми");
-        }
+    public List<StatsViewDto> getStats(ClientRequestDto сlientRequestDto) {
 
-        if (end.isBefore(start)) {
+        if (сlientRequestDto.getEnd().isBefore(сlientRequestDto.getStart())) {
             throw new IllegalArgumentException("Ошибка валидации. Дата окончания не может быть раньше даты начала");
         }
 
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
                 .fromPath("/stats")
-                .queryParam("start", start.format(DateTimeFormatter.ofPattern(DateTimeFormats.FORMATTER)))
-                .queryParam("end", end.format(DateTimeFormatter.ofPattern(DateTimeFormats.FORMATTER)));
+                .queryParam("start", сlientRequestDto.getStart().format(DateTimeFormatter.ofPattern(DateTimeFormats.FORMATTER)))
+                .queryParam("end", сlientRequestDto.getEnd().format(DateTimeFormatter.ofPattern(DateTimeFormats.FORMATTER)));
 
-        if (uris != null && !uris.isEmpty()) {
-            uriComponentsBuilder.queryParam("uris", uris);
+        if (сlientRequestDto.getUris() != null && !сlientRequestDto.getUris().isEmpty()) {
+            uriComponentsBuilder.queryParam("uris", сlientRequestDto.getUris());
         }
 
-        if (unique != null) {
-            uriComponentsBuilder.queryParam("unique", unique);
+        if (сlientRequestDto.getUnique() != null) {
+            uriComponentsBuilder.queryParam("unique", сlientRequestDto.getUnique());
         }
 
         return restClient.get()
---
+++ stats/client/src/main/java/ru/practicum/ewm/stats/client/ClientRequestDto.java
@@ -0,0 +1,25 @@
+package ru.practicum.ewm.stats.client;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientRequestDto {
+    @NotNull(message = "Ошибка валидации. Дата начала не может быть пустой")
+    @FutureOrPresent
+    private LocalDateTime start;
+    @NotNull(message = "Ошибка валидации. Дата окончания не может быть пустой")
+    @Future
+    private LocalDateTime end;
+    private List<String> uris;
+    private Boolean unique;
+}

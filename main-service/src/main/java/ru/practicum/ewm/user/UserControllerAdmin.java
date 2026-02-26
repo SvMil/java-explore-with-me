74223e53c3c77681635dff72b0e83bc18d508f78
SvMil <lana.milovidova@yandex.ru>
2026-02-27 01:07:00 +0300

    add main_svc branch

--- main-service/src/main/java/ru/practicum/ewm/user/UserControllerAdmin.java
+++ main-service/src/main/java/ru/practicum/ewm/user/UserControllerAdmin.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.user;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.user.dto.UserDto;
 import ru.practicum.ewm.user.service.UserService;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/users")
 @RequiredArgsConstructor
+@Validated
 public class UserControllerAdmin {
     private final UserService service;
 

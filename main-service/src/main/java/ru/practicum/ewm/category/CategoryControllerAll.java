652734aaa520cb6186299eee1cb66050076debe3
SvMil <lana.milovidova@yandex.ru>
2026-02-26 17:06:03 +0300

    add main_svc branch

---
+++ main-service/src/main/java/ru/practicum/ewm/category/CategoryControllerAll.java
@@ -0,0 +1,33 @@
+package ru.practicum.ewm.category;
+
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
+import ru.practicum.ewm.category.service.CategoryService;
+import ru.practicum.ewm.category.dto.CategoryDto;
+
+import java.util.List;
+
+
+@RequestMapping("/categories")
+@RestController
+@RequiredArgsConstructor
+public class CategoryControllerAll {
+    private final CategoryService service;
+
+    @GetMapping("/{catId}")
+    public CategoryDto getCategoryById(
+            @PathVariable long catId
+    ) {
+        return service.getById(catId);
+    }
+
+    @GetMapping
+    public List<CategoryDto> getCategories(
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return service.getAll(from, size);
+    }
+}

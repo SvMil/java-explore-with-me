41787a060077f8e1c2e128c7a0ab3525a9f09705
SvMil <lana.milovidova@yandex.ru>
2026-02-27 18:02:24 +0300

    add main_svc branch

--- main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
@@ -24,7 +24,6 @@ import ru.practicum.ewm.category.repository.CategoryRepository;
 import ru.practicum.ewm.request.repository.RequestRepository;
 import ru.practicum.ewm.user.repository.UserRepository;
 import ru.practicum.ewm.stats.client.Client;
-import ru.practicum.ewm.stats.dto.ClientRequestDto;
 import ru.practicum.ewm.stats.dto.HitEndpointDto;
 import ru.practicum.ewm.stats.dto.StatsViewDto;
 

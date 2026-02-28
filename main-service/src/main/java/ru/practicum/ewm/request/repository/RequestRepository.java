e9cab9ba4ed3819a3742b8c3a1ae33b9cd19950a
SvMil <lana.milovidova@yandex.ru>
2026-02-28 14:52:39 +0300

    fix main_svc branch

--- main-service/src/main/java/ru/practicum/ewm/request/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/request/repository/RequestRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.request.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.enums.RequestStatus;
 import ru.practicum.ewm.request.model.Request;
